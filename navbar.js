aab6eb4ee17c1036927e1ff1384708c535bac4dc
ammmambaje <ammmambaje@addu.edu.ph>
2024-12-08 02:33:35 +0800

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,17 @@
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+
+// Handle scroll event to hide/show navbar
+window.addEventListener('scroll', function() {
+   let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+   if (currentScroll > lastScrollTop) {
+       // Scrolling down
+       navbar.style.top = "-60px"; 
+   } else {
+       // Scrolling up
+       navbar.style.top = "0"; // Show navbar
+   }
+
+   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+});
